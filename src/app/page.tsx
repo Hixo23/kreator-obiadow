c87809c30ce7c28d513e3666e6e4e194e620bf5a
Hixo <hixohixo32@gmail.com>
2024-11-06 07:35:00 +0100

    feature: change bg color in dark theme

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ import { HydrateClient } from "@/trpc/server";
 export default async function Home() {
   return <HydrateClient>
     <div className="w-screen h-screen font-poppins">
-      <main className="dark:bg-sidebar dark:text-white">
+      <main className="dark:text-white">
         <MealSidebar>
           <MealList />
         </MealSidebar>
