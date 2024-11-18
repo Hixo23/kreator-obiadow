0ccb91a0d930e49abbbf235a7d1edbf8eb24a341
Hixo <hixohixo32@gmail.com>
2024-11-18 12:11:04 +0100

    fix: add correct path to global css stylesheet

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,4 @@
-import "@/styles/globals.css";
+import "@/shared/styles/globals.css";
 import { Poppins } from "next/font/google";
 import { type Metadata } from "next";
 import { TRPCReactProvider } from "@/trpc/react";
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,15 +3,17 @@ import { MealList } from "@/features/meals/components/meal-list/meal-list";
 import { api, HydrateClient } from "@/trpc/server";
 
 export default async function Home() {
-  const meals = await api.recipe.getAll()
-  return <HydrateClient>
-    <div className="w-screen h-screen font-poppins">
-      <main className="dark:text-white">
-        <div className="flex flex-col w-full items-center">
-          <MealFilter meals={meals} />
-          <MealList meals={meals} />
-        </div>
-      </main>
-    </div>
-  </HydrateClient>
+  const meals = await api.recipe.getAll();
+  return (
+    <HydrateClient>
+      <div className="h-screen w-screen font-poppins">
+        <main className="flex h-full w-full dark:text-white">
+          <div className="flex w-full flex-col items-center">
+            <MealFilter meals={meals} />
+            <MealList meals={meals} />
+          </div>
+        </main>
+      </div>
+    </HydrateClient>
+  );
 }
