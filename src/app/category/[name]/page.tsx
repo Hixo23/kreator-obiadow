34c4fe9ca9271cb1dccdb949b92705b216c5a6f5
Hixo <hixohixo32@gmail.com>
2024-11-19 13:29:26 +0100

    fix: add promise in params type

--- src/app/category/[name]/page.tsx
+++ src/app/category/[name]/page.tsx
@@ -1,13 +1,19 @@
 import { MealList } from "@/features/meals/components/meal-list/meal-list";
 import { api } from "@/trpc/server";
 
-const MealCategoryPage = async ({ params }: { params: { name: string } }) => {
-  const meals = await api.recipe.getByCategory({ category: params.name });
+const MealCategoryPage = async ({
+  params,
+}: {
+  params: Promise<{ name: string }>;
+}) => {
+  const meals = await api.recipe.getByCategory({
+    category: (await params).name,
+  });
   return (
     <main className="flex h-full w-full flex-col items-center justify-center dark:text-white">
       <h1 className="mx-auto text-nowrap py-24 text-center text-5xl font-semibold uppercase">
-        {params.name.charAt(0).toUpperCase() +
-          params.name.replace("-", " ").slice(1)}
+        {(await params).name.charAt(0).toUpperCase() +
+          (await params).name.replace("-", " ").slice(1)}
       </h1>
       <div className="flex w-full flex-col items-center">
         <MealList meals={meals} />
--- src/app/meal/[id]/page.tsx
+++ src/app/meal/[id]/page.tsx
@@ -1,12 +1,11 @@
+import { Meal as MealComponent } from "@/features/meals/meal";
+import { api } from "@/trpc/server";
+import { type MealProps } from "@/features/meals/meal";
 
-import { Meal as MealComponent } from "@/features/meals/meal"
-import { api } from "@/trpc/server"
-import { type MealProps } from "@/features/meals/meal"
+const MealPage = async ({ params }: { params: Promise<{ id: string }> }) => {
+  const meal = await api.recipe.getById({ id: (await params).id });
 
-const MealPage = async ({ params }: { params: { id: string } }) => {
-  const meal = await api.recipe.getById({ id: params.id })
-
-  if (!meal[0]) return
+  if (!meal[0]) return;
 
   const mealData: MealProps = {
     name: meal[0].name,
@@ -15,10 +14,10 @@ const MealPage = async ({ params }: { params: { id: string } }) => {
     preparationTime: meal[0].preparationTime,
     portions: meal[0].portions,
     ingredients: meal[0].ingredients.split(","),
-    preparationProcess: meal[0].preparationProcess
+    preparationProcess: meal[0].preparationProcess,
   };
 
-  return <MealComponent {...mealData} />
-}
+  return <MealComponent {...mealData} />;
+};
 
-export default MealPage
+export default MealPage;
