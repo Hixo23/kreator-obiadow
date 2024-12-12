0d1b5279cb15b020487708111c92d9ae14764e8d
Hixo <hixohixo32@gmail.com>
2024-12-12 18:13:11 +0100

    feat: add deleting comment

--- src/features/comments/components/comments-list/comments-list.tsx
+++ src/features/comments/components/comments-list/comments-list.tsx
@@ -1,12 +1,12 @@
 import { ScrollArea } from "@radix-ui/react-scroll-area";
 import { SingleComment } from "../single-comment/single-comment";
 
-export type MappedComment = { content: string, author: { username: string, avatar: string } }
+export type MappedComment = { content: string, author: { username: string, avatar: string }, id: string }
 
 export const CommentsList = ({ comments }: { comments: MappedComment[] }) => {
   return <ScrollArea>
     {comments.length >= 1 ? comments.map((comment, i) => (
-      <SingleComment key={i} comment={comment} />
+      <SingleComment key={comment.id} comment={comment} />
     )) : null}
   </ScrollArea>
 }
--- src/features/comments/components/single-comment/single-comment.tsx
+++ src/features/comments/components/single-comment/single-comment.tsx
@@ -1,8 +1,11 @@
 import { Avatar, AvatarFallback, AvatarImage } from "@radix-ui/react-avatar"
 import { type MappedComment } from "../comments-list/comments-list"
+import { CommentControls } from "../comment-control/comment-control"
+import { Button } from "@/shared/components/ui/shadcn/button"
+import { Ellipsis } from "lucide-react"
 
 export const SingleComment = ({ comment }: { comment: MappedComment }) => {
-  return <div className="flex items-start space-x-4 mb-4">
+  return <div className="flex relative items-start w-full space-x-4 mb-4">
     <Avatar>
       <AvatarImage width={64} className="rounded-full" src={comment.author.avatar} alt={comment.author.username} />
       <AvatarFallback>{comment.author.username}</AvatarFallback>
@@ -11,5 +14,8 @@ export const SingleComment = ({ comment }: { comment: MappedComment }) => {
       <p className="font-semibold">{comment.author.username}</p>
       <p className="text-muted-foreground">{comment.content}</p>
     </div>
+    <CommentControls commentId={comment.id}>
+      <Button className="absolute right-5"><Ellipsis /></Button>
+    </CommentControls>
   </div>
 }
