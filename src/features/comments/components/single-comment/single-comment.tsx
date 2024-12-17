3828ce908381938ce2274e2d56536db091248582
Hixo <hixohixo32@gmail.com>
2024-12-17 20:33:49 +0100

    fix: checking user is comment author

--- src/features/comments/components/single-comment/single-comment.tsx
+++ src/features/comments/components/single-comment/single-comment.tsx
@@ -3,8 +3,10 @@ import { type MappedComment } from "../comments-list/comments-list"
 import { CommentControls } from "../comment-control/comment-control"
 import { Button } from "@/shared/components/ui/shadcn/button"
 import { Ellipsis } from "lucide-react"
+import {currentUser} from "@clerk/nextjs/server";
 
-export const SingleComment = ({ comment }: { comment: MappedComment }) => {
+export const SingleComment = async ({ comment }: { comment: MappedComment }) => {
+  const user = await currentUser()
   return <div className="flex relative items-start w-full space-x-4 mb-4">
     <Avatar>
       <AvatarImage width={64} className="rounded-full" src={comment.author.avatar} alt={comment.author.username} />
@@ -14,8 +16,8 @@ export const SingleComment = ({ comment }: { comment: MappedComment }) => {
       <p className="font-semibold">{comment.author.username}</p>
       <p className="text-muted-foreground">{comment.content}</p>
     </div>
-    <CommentControls commentId={comment.id}>
+    {user?.username === comment.author.username ? <CommentControls commentId={comment.id}>
       <Button className="absolute right-5"><Ellipsis /></Button>
-    </CommentControls>
+    </CommentControls> : null}
   </div>
 }
