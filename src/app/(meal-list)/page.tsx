2dd66470c6245d6ff2001af43e11e58284d776bb
Hixo <hixohixo32@gmail.com>
2024-12-18 21:11:28 +0100

    feat: add cachedmeals

--- src/app/(meal-list)/page.tsx
+++ src/app/(meal-list)/page.tsx
@@ -1,9 +1,12 @@
 import { MealFilter } from "@/features/filtering/components/meal-filter/meal-filter";
 import { MealList } from "@/features/meals/components/meal-list/meal-list";
 import { mealRepository } from "@/server/repositiries/meals";
+import { unstable_cache } from "next/cache";
+
+const getCachedMeals = unstable_cache(async () => await mealRepository.findMany(), ["meals"])
 
 export default async function Home() {
-  const meals = await mealRepository.findMany();
+  const meals = await getCachedMeals()
   return (
     <div className="h-screen w-screen font-poppins">
       <main className="flex h-full w-full dark:text-white">
