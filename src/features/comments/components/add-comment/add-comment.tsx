5e2522f12ee938640e5ef5dc38a69c3518696b85
Hixo <hixohixo32@gmail.com>
2025-02-15 17:49:24 +0100

    refactor: rewrite add component hook to rhf

--- src/features/comments/components/add-comment/add-comment.tsx
+++ src/features/comments/components/add-comment/add-comment.tsx
@@ -4,24 +4,41 @@ import { Button } from "@/shared/components/ui/shadcn/button";
 import { Input } from "@/shared/components/ui/shadcn/input";
 import { useState } from "react";
 import { addComment } from "../../actions/add-comment";
+import { useUser } from "@clerk/nextjs";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+const commentFormSchema = z.object({
+  comment: z
+    .string()
+    .min(1, { message: "Komentarz powinien być dłuższy niz 1 znak" }),
+});
 
 export const AddComment = ({ postId }: { postId: string }) => {
-  const [newComment, setNewComment] = useState("");
-  const onSubmit = async (evt: React.FormEvent) => {
-    evt.preventDefault();
-    await addComment({ content: newComment, postId: postId });
-    setNewComment("");
-  };
+  const user = useUser();
+  const { register, handleSubmit, setValue } = useForm({
+    resolver: zodResolver(commentFormSchema),
+    defaultValues: {
+      comment: "",
+    },
+  });
+  const onSubmit = handleSubmit(async (data: { comment: string }) => {
+    await addComment({ content: data.comment, postId: postId });
+    return setValue("comment", "");
+  });
   return (
     <form onSubmit={onSubmit} className="mb-6 flex w-full space-x-2">
       <Input
         type="text"
         placeholder="Dodaj komentarz"
-        value={newComment}
-        onChange={(e) => setNewComment(e.target.value)}
+        disabled={!user.isSignedIn}
+        {...register("comment")}
         className="flex-grow"
       />
-      <Button type="submit">Dodaj</Button>
+      <Button disabled={!user.isSignedIn} type="submit">
+        Dodaj
+      </Button>
     </form>
   );
 };
