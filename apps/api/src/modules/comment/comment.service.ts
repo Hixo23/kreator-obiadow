03c584fe3a1c4f46f136197b4a79794fc7f8cf89
Hixo <hixohixo32@gmail.com>
2025-03-11 12:25:03 +0100

    feat: add checking comment is exists

--- apps/api/src/modules/comment/comment.service.ts
+++ apps/api/src/modules/comment/comment.service.ts
@@ -1,29 +1,45 @@
-import { BadRequestException, Injectable, InternalServerErrorException } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  InternalServerErrorException,
+  NotFoundException,
+} from '@nestjs/common';
 import { CreateCommentDto } from './dto/create-comment.dto';
 import { UpdateCommentDto } from './dto/update-comment.dto';
 import { PrismaService } from 'src/prisma.service';
 
 @Injectable()
 export class CommentService {
-  constructor(private readonly prismaService: PrismaService) { }
+  constructor(private readonly prismaService: PrismaService) {}
   async create({ authorId, content, rating, recipeId }: CreateCommentDto) {
-    if (!authorId || !content || !rating || !recipeId) throw new BadRequestException();
+    if (!authorId || !content || !rating || !recipeId)
+      throw new BadRequestException();
     const comment = await this.prismaService.comment.create({
       data: {
         content,
         rating,
         author: {
           connect: {
-            id: authorId
-          }
+            id: authorId,
+          },
         },
         recipe: {
           connect: {
-            id: recipeId
-          }
-        }
-      }
-    })
+            id: recipeId,
+          },
+        },
+      },
+    });
+
+    return comment;
+  }
+
+  async findOne(id: string) {
+    const comment = await this.prismaService.comment.findFirst({
+      where: {
+        id,
+      },
+    });
 
     return comment;
   }
@@ -31,24 +47,26 @@ export class CommentService {
   async findAll(id: string) {
     const comments = await this.prismaService.comment.findMany({
       where: {
-        recipeId: id
-      }
-    })
+        recipeId: id,
+      },
+    });
 
     return comments;
   }
 
-
   async update(id: string, { rating, content }: UpdateCommentDto) {
+    const commentExists = await this.findOne(id);
+
+    if (!commentExists) throw new NotFoundException();
     const updatedComment = await this.prismaService.comment.update({
       where: {
-        id
+        id,
       },
       data: {
         content,
-        rating
-      }
-    })
+        rating,
+      },
+    });
 
     if (!updatedComment) throw new InternalServerErrorException();
 
@@ -56,12 +74,15 @@ export class CommentService {
   }
 
   async remove(id: string) {
+    const commentExists = await this.findOne(id);
+
+    if (!commentExists) throw new NotFoundException();
     await this.prismaService.comment.delete({
       where: {
-        id
-      }
-    })
+        id,
+      },
+    });
 
-    return { message: "comment deleted" }
+    return { message: 'comment deleted' };
   }
 }
