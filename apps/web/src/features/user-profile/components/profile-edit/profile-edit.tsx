33575f5866a6a5ce8aebcc82a799fcd97b089618
Hixo <hixohixo32@gmail.com>
2025-04-04 18:04:15 +0200

    feat: change contnet of save button in profile edit

--- apps/web/src/features/user-profile/components/profile-edit/profile-edit.tsx
+++ apps/web/src/features/user-profile/components/profile-edit/profile-edit.tsx
@@ -1,71 +1,103 @@
-import { Button } from "@/shared/components/ui/shadcn/button"
-import { Dialog, DialogContent, DialogHeader, DialogTitle, DialogTrigger } from "@/shared/components/ui/shadcn/dialog"
-import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/shared/components/ui/shadcn/form"
-import { Input } from "@/shared/components/ui/shadcn/input"
-import { Textarea } from "@/shared/components/ui/shadcn/textarea"
-import { IProfile } from "@/shared/types"
-import { zodResolver } from "@hookform/resolvers/zod"
-import { useForm } from "react-hook-form"
-import { z } from "zod"
-import { useEditProfile } from "../../hooks/use-edit-profile"
+import { Button } from "@/shared/components/ui/shadcn/button";
+import {
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/shared/components/ui/shadcn/dialog";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/shared/components/ui/shadcn/form";
+import { Input } from "@/shared/components/ui/shadcn/input";
+import { Textarea } from "@/shared/components/ui/shadcn/textarea";
+import { IProfile } from "@/shared/types";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { useEditProfile } from "../../hooks/use-edit-profile";
 
 const editProfileSchema = z.object({
-  username: z.string().min(4, { message: "Twoja nazwa użytkownika nie moze być krótsza niz 4 znaki " }),
-  description: z.string().min(5, { "message": "Twój opis nie może być krótszy niz 5 znaków" }).optional()
-})
+  username: z.string().min(4, {
+    message: "Twoja nazwa użytkownika nie moze być krótsza niz 4 znaki ",
+  }),
+  description: z
+    .string()
+    .min(5, { message: "Twój opis nie może być krótszy niz 5 znaków" })
+    .optional(),
+});
 
-export const ProfileEdit = ({ children, profile, setIsOpen, isOpen }: { children: React.ReactNode, profile: IProfile, isOpen: boolean, setIsOpen: React.Dispatch<React.SetStateAction<boolean>> }) => {
+export const ProfileEdit = ({
+  children,
+  profile,
+  setIsOpen,
+  isOpen,
+}: {
+  children: React.ReactNode;
+  profile: IProfile;
+  isOpen: boolean;
+  setIsOpen: React.Dispatch<React.SetStateAction<boolean>>;
+}) => {
   const form = useForm({
     resolver: zodResolver(editProfileSchema),
     defaultValues: {
       description: profile.description,
-      username: profile.username
-    }
-  })
+      username: profile.username,
+    },
+  });
 
-  const { mutate, isSuccess } = useEditProfile()
+  const { mutate, isError, isPending } = useEditProfile();
 
   const onSubmit = (values: z.infer<typeof editProfileSchema>) => {
     mutate(values);
-    if (isSuccess) setIsOpen(false);
-  }
-  return <Dialog open={isOpen} onOpenChange={setIsOpen}>
-    <DialogTrigger asChild>{children}</DialogTrigger>
-    <DialogContent>
-      <DialogHeader>
-        <DialogTitle>Edytuj profil</DialogTitle>
-      </DialogHeader>
-      <Form {...form}>
-        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-          <FormField
-            control={form.control}
-            name="username"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>Nazwa użytkownika</FormLabel>
-                <FormControl>
-                  <Input placeholder="elorzelo" {...field} />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-          <FormField
-            control={form.control}
-            name="description"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>Opis</FormLabel>
-                <FormControl>
-                  <Textarea placeholder="Zaawansowany kucharz" {...field} />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-          <Button type="submit">Submit</Button>
-        </form>
-      </Form>
-    </DialogContent>
-  </Dialog>
-}
+    if (!isError) setIsOpen(false);
+  };
+  return (
+    <Dialog open={isOpen} onOpenChange={setIsOpen}>
+      <DialogTrigger asChild>{children}</DialogTrigger>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>Edytuj profil</DialogTitle>
+        </DialogHeader>
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+            <FormField
+              control={form.control}
+              name="username"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Nazwa użytkownika</FormLabel>
+                  <FormControl>
+                    <Input placeholder="elorzelo" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="description"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Opis</FormLabel>
+                  <FormControl>
+                    <Textarea placeholder="Zaawansowany kucharz" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <Button disabled={isPending} type="submit">
+              Zapisz
+            </Button>
+          </form>
+        </Form>
+      </DialogContent>
+    </Dialog>
+  );
+};
